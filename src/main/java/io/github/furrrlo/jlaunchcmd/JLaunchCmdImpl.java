802464b220f089c5314880295a31b39be7eb1fb3
Furrrlo <ferloo@protonmail.com>
2021-12-15 00:35:16 +0100

    Reduce code duplication in JLaunchCmdImpl

--- src/main/java/io/github/furrrlo/jlaunchcmd/JLaunchCmdImpl.java
+++ src/main/java/io/github/furrrlo/jlaunchcmd/JLaunchCmdImpl.java
@@ -3,8 +3,8 @@ package io.github.furrrlo.jlaunchcmd;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class JLaunchCmdImpl implements JLaunchCmd {
 
@@ -18,51 +18,30 @@ class JLaunchCmdImpl implements JLaunchCmd {
 
     @Override
     public String[] getLaunchCommand() {
-        final List<Throwable> exceptions = new ArrayList<>();
-
-        for(JLaunchCmdService service : services) {
-            try {
-                return service.tryGetLaunchCommand();
-            } catch (Exception ex) {
-                if(LOGGER.isLoggable(Level.FINE))
-                    LOGGER.log(Level.FINE, "Service " + service + " failed", ex);
-
-                exceptions.add(ex);
-            }
-        }
-
-        final RuntimeException ex = new RuntimeException();
-        exceptions.forEach(ex::addSuppressed);
-        throw ex;
+        return tryForEachService(JLaunchCmdService::tryGetLaunchCommand);
     }
 
     @Override
     public String getShellLaunchCommand() {
-        final List<Throwable> exceptions = new ArrayList<>();
-
-        for(JLaunchCmdService service : services) {
-            try {
-                return service.tryGetShellLaunchCommand();
-            } catch (Exception ex) {
-                if(LOGGER.isLoggable(Level.FINE))
-                    LOGGER.log(Level.FINE, "Service " + service + " failed", ex);
-
-                exceptions.add(ex);
-            }
-        }
-
-        final RuntimeException ex = new RuntimeException();
-        exceptions.forEach(ex::addSuppressed);
-        throw ex;
+        return tryForEachService(JLaunchCmdService::tryGetShellLaunchCommand);
     }
 
     @Override
     public String getExecutable() {
+        return tryForEachService(JLaunchCmdService::tryGetExecutable);
+    }
+
+    @Override
+    public String[] getArguments() {
+        return tryForEachService(JLaunchCmdService::tryGetArguments);
+    }
+
+    private <T, E extends Exception> T tryForEachService(FunctionWithException<JLaunchCmdService, T, E> func) throws RuntimeException {
         final List<Throwable> exceptions = new ArrayList<>();
 
         for(JLaunchCmdService service : services) {
             try {
-                return service.tryGetExecutable();
+                return func.apply(service);
             } catch (Exception ex) {
                 if(LOGGER.isLoggable(Level.FINE))
                     LOGGER.log(Level.FINE, "Service " + service + " failed", ex);
@@ -76,23 +55,8 @@ class JLaunchCmdImpl implements JLaunchCmd {
         throw ex;
     }
 
-    @Override
-    public String[] getArguments() {
-        final List<Throwable> exceptions = new ArrayList<>();
-
-        for(JLaunchCmdService service : services) {
-            try {
-                return service.tryGetArguments();
-            } catch (Exception ex) {
-                if(LOGGER.isLoggable(Level.FINE))
-                    LOGGER.log(Level.FINE, "Service " + service + " failed", ex);
-
-                exceptions.add(ex);
-            }
-        }
-
-        final RuntimeException ex = new RuntimeException();
-        exceptions.forEach(ex::addSuppressed);
-        throw ex;
+    @FunctionalInterface
+    private interface FunctionWithException<T, R, E extends Throwable> {
+        R apply(T t) throws E;
     }
 }
